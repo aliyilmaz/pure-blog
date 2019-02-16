99e8ae9c5465552b4f1a074a7c4401f7cc1cd563
aliyilmaz <aliyilmaz.work@gmail.com>
2019-02-16 19:35:57 +0300

    Route descriptions added.

--- index.php
+++ index.php
@@ -4,8 +4,11 @@ require_once('Mind.php');
 use Mind\Mind;
 $Mind = new Mind();
 
+// Static page routes.
 $Mind->route('/', 'app/views/index');
 $Mind->route('admin', 'app/views/admin');
+
+// Table creation routes.
 $Mind->route('install/users', 'app/views/index', 'app/tables/users');
 $Mind->route('install/posts', 'app/views/index', 'app/tables/posts');
 $Mind->route('install/general', 'app/views/index', 'app/tables/general');
