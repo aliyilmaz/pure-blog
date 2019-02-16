6393d00ae715410d639ad395a2a02ce7b05a1b9a
aliyilmaz <aliyilmaz.work@gmail.com>
2019-02-16 20:07:02 +0300

    Added content creator route.

--- index.php
+++ index.php
@@ -12,3 +12,6 @@ $Mind->route('admin', 'app/views/admin');
 $Mind->route('install/users', 'app/views/index', 'app/tables/users');
 $Mind->route('install/posts', 'app/views/index', 'app/tables/posts');
 $Mind->route('install/general', 'app/views/index', 'app/tables/general');
+
+// Demo content creation routes.
+$Mind->route('tests/post-add', 'app/views/index', 'app/tests/add_post');
