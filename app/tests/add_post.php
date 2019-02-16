64cfce7b1ddbce0d0671704d359a23a251c3bab5
aliyilmaz <aliyilmaz.work@gmail.com>
2019-02-17 00:28:58 +0300

    Content was saved with HTML code.

--- app/tests/add_post.php
+++ app/tests/add_post.php
@@ -18,18 +18,27 @@ $scheme = array(
     array(
         'title'=>'Photos from CSSConf and JSConf',
         'content'=>json_encode(
-            array(
-                array(
-                    'title'=>'CSSConf Photos', 
-                    'img'=>'http://farm8.staticflickr.com/7448/8915936174_8d54ec76c6.jpg',
-                    'href'=>'http://www.flickr.com/photos/uberlife/8915936174/'
-                ),
-                array(
-                    'title'=>'JSConf Photos', 
-                    'img'=>'http://farm8.staticflickr.com/7382/8907351301_bd7460cffb.jpg',
-                    'href'=>'http://www.flickr.com/photos/uberlife/8907351301/'
-                )
-            )
+            array('<div class="post-images pure-g">
+            <div class="pure-u-1 pure-u-md-1-2">
+                <a href="http://www.flickr.com/photos/uberlife/8915936174/">
+                    <img alt="Photo of someone working poolside at a resort" class="pure-img-responsive" src="http://farm8.staticflickr.com/7448/8915936174_8d54ec76c6.jpg">
+                </a>
+
+                <div class="post-image-meta">
+                    <h3>CSSConf Photos</h3>
+                </div>
+            </div>
+
+            <div class="pure-u-1 pure-u-md-1-2">
+                <a href="http://www.flickr.com/photos/uberlife/8907351301/">
+                    <img alt="Photo of the sunset on the beach" class="pure-img-responsive" src="http://farm8.staticflickr.com/7382/8907351301_bd7460cffb.jpg">
+                </a>
+
+                <div class="post-image-meta">
+                    <h3>JSConf Photos</h3>
+                </div>
+            </div>
+        </div>')
         ),
         'tags'=>json_encode(array('Uncategorized')),
         'created_at'=>date('d-m-Y H:i:s'),
