d4d042eb565cdf6b748f464cf79953d156451cb8
aliyilmaz <aliyilmaz.work@gmail.com>
2019-02-16 21:18:58 +0300

    The test user generator was created.

---
+++ app/tests/add_user.php
@@ -0,0 +1,40 @@
+<?php
+
+$scheme = array(
+    array(
+        'username'=>'Tilo Mitra',
+        'password'=>md5('123456'),
+        'email'=>'tilo.mitra@example.com',
+        'avatar'=>'public/img/common/tilo-avatar.png',
+        '_token'=>md5(rand(999,9999)),
+        'created_at'=>date('d-m-Y H:i:s')
+    ),
+    array(
+        'username'=>'Eric Ferraiuolo',
+        'password'=>md5('123456'),
+        'email'=>'eric.ferraiuolo@example.com',
+        'avatar'=>'public/img/common/ericf-avatar.png',
+        '_token'=>md5(rand(999,9999)),
+        'created_at'=>date('d-m-Y H:i:s')
+    ),
+    array(
+        'username'=>'Reid Burke',
+        'password'=>md5('123456'),
+        'email'=>'reid.burke@example.com',
+        'avatar'=>'public/img/common/reid-avatar.png',
+        '_token'=>md5(rand(999,9999)),
+        'created_at'=>date('d-m-Y H:i:s')
+    ),
+    array(
+        'username'=>'Reid Burke',
+        'password'=>md5('123456'),
+        'email'=>'reid.burke@example.com',
+        'avatar'=>'public/img/common/andrew-avatar.png',
+        '_token'=>md5(rand(999,9999)),
+        'created_at'=>date('d-m-Y H:i:s')
+    )
+);
+
+$this->insert('users', $scheme);
+
+?>
