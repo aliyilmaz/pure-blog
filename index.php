95870f32e10dd023283e62d8af612a1e6e10d226
aliyilmaz <aliyilmaz.work@gmail.com>
2019-02-18 17:09:21 +0300

    Category route added.

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ $Mind->route('install/categories', 'app/views/index', 'app/tables/categories');
 $Mind->route('tests/post-add', 'app/views/index', 'app/tests/add_post');
 $Mind->route('tests/user-add', 'app/views/index', 'app/tests/add_user');
 $Mind->route('tests/general-add', 'app/views/index', 'app/tests/add_general');
+$Mind->route('tests/category-add', 'app/views/index', 'app/tests/add_category');
 
 // Data collection routes.
 $Mind->route('tests/posts', 'app/tests/data_posts');
