7c96e766fb3aa274df8bf2890caeddf157a8fe7e
aliyilmaz <aliyilmaz.work@gmail.com>
2019-02-18 17:08:31 +0300

    Category insertion test added.

---
+++ app/tests/add_category.php
@@ -0,0 +1,26 @@
+<?php
+
+$data = array(
+    array(
+        'text'=>'Uncategorized', 
+        'class_name'=>'post-category', 
+        'created_at'=>date('d-m-Y H:i:s')
+    ),
+    array(
+        'text'=>'CSS', 
+        'class_name'=>'post-category-design', 
+        'created_at'=>date('d-m-Y H:i:s')
+    ),
+    array(
+        'text'=>'JavaScript', 
+        'class_name'=>'post-category-js', 
+        'created_at'=>date('d-m-Y H:i:s')
+    ),
+    array(
+        'text'=>'YUI', 
+        'class_name'=>'post-category-yui', 
+        'created_at'=>date('d-m-Y H:i:s')
+    )
+);
+
+$this->insert('categories', $data);
