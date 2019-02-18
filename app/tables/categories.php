d0c7aa94b178e73ad10da7df74b253c9a60d96d6
aliyilmaz <aliyilmaz.work@gmail.com>
2019-02-18 14:55:09 +0300

    Categories table has been added.

---
+++ app/tables/categories.php
@@ -0,0 +1,42 @@
+<?php
+
+$scheme = array(
+    'id:increments',
+    'parent:string',
+    'text:string',
+    'href:string',
+    'description:small',
+    'class_name:string',
+    'created_at:string',
+    'updated_at:string'
+);
+
+$this->createtable('categories', $scheme);
+
+// Added demo categories.
+$data = array(
+    array(
+        'text'=>'Uncategorized', 
+        'class_name'=>'post-category', 
+        'created_at'=>date('d-m-Y H:i:s')
+    ),
+    array(
+        'text'=>'CSS', 
+        'class_name'=>'post-category-design', 
+        'created_at'=>date('d-m-Y H:i:s')
+    ),
+    array(
+        'text'=>'JavaScript', 
+        'class_name'=>'post-category-js', 
+        'created_at'=>date('d-m-Y H:i:s')
+    ),
+    array(
+        'text'=>'YUI', 
+        'class_name'=>'post-category-yui', 
+        'created_at'=>date('d-m-Y H:i:s')
+    )
+);
+
+$this->insert('categories', $data);
+
+?>
