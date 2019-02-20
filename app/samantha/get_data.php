82f9c9ebd00f9ab373a8682783039f1ed272ee34
aliyilmaz <aliyilmaz.work@gmail.com>
2019-02-21 00:11:44 +0300

    A general data retrieval function has been created.

---
+++ app/samantha/get_data.php
@@ -0,0 +1,25 @@
+<?php
+
+function get_data($table, $needle, $column=null){
+    
+    global $Mind;
+    $data = array();
+    
+    if(!$Mind->is_column($table, $column)){
+        $column = $Mind->increments($table);
+    }
+    
+    $schema = array(
+        'search'=>array(
+            'equal'=>array($column=>$needle)
+        )
+    );
+
+    $data = $Mind->get($table, $schema);
+
+    if(count($data)>1){
+        return $data;
+    }
+
+    return $data[0];
+}
