a6ea295e706c9228cb9e60b03e6f6963591a6e38
aliyilmaz <aliyilmaz.work@gmail.com>
2019-03-06 00:56:30 +0300

    Vuejs started to read dynamic content.

--- app/views/vueindex.php
+++ app/views/vueindex.php
@@ -1,40 +1,154 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <base href="<?=$this->baseurl; ?>">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="description" content="A layout example that shows off a blog page with a list of posts.">
+    <title>Blog &ndash; Layout Examples &ndash; Pure</title>
+    
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-" crossorigin="anonymous">
+    
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-old-ie-min.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-min.css">
+    <!--<![endif]-->
+    
+    
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="public/css/layouts/blog-old-ie.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+    <link rel="stylesheet" href="public/css/layouts/blog.css">
+    <!--<![endif]-->
+
     <!-- production version, optimized for size and speed -->
     <script src="https://cdn.jsdelivr.net/npm/vue"></script>
 </head>
 <body>
-    <div id="app">
 
-    <ul>
-        <li v-for="post in posts">
-        <h1 v-if="post.pinstatus">pinned - {{ post.title }}</h1>
-        <h1 v-else>no pinned - {{ post.title }}</h1>
-        
-        </li>
-    </ul>
+
+
+
+
+
+
+<div id="layout" class="pure-g">
+    <div class="sidebar pure-u-1 pure-u-md-1-4">
+        <div id="general" class="header">
+            <h1 class="brand-title">{{ title }}</h1>
+            <h2 class="brand-tagline">{{ description }}</h2>
+
+            <nav class="nav">
+                <ul class="nav-list">
+                    <li class="nav-item" v-for="button in buttons">
+                        <a class="pure-button" v-bind:href="button.href">{{ button.text }}</a> 
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
-    <script>
-        window.addEventListener('load', () => {
-            new Vue({
-                el: '#app',
-                data:{
-                    posts: []
-                },
-                created(){
-                    fetch('api/posts')
-                    .then((res) => {return res.json()})
-                    .then((res) => {
-                        this.posts = res;
-                    })
-                }
-            })
-        });
+
+    <div class="content pure-u-1 pure-u-md-3-4">
+        <div>
+            <div v-for="post in posts">
+                <!-- A wrapper for all the blog posts -->
+                <div class="posts" v-if="post.pinstatus">
+                    <h1 class="content-subhead">Pinned Post</h1>
+
+                    <!-- A single blog post -->
+                    <section class="post">
+                        <header class="post-header">
+                            <img width="48" height="48" alt="Author &#x27;s avatar" class="post-avatar" src="img/common/tilo-avatar.png">
+
+                            <h2 class="post-title">{{ post.title }}</h2>
+
+                            <p class="post-meta">
+                                By <a href="#" class="post-author">{{ post.author }}</a> under <a class="post-category post-category-design" href="#">CSS</a> <a class="post-category post-category-pure" href="#">Pure</a>
+                            </p>
+                        </header>
+
+                        <div class="post-description">
+                            <p>
+                            {{ post.content }}
+                            </p>
+                        </div>
+                    </section>
+                </div>
+
+                <div class="posts" v-else="post.pinstatus">
+                    <h1 class="content-subhead">Recent Posts</h1>
+
+                    <section class="post">
+                        <header class="post-header">
+                            <img width="48" height="48" alt="Eric Ferraiuolo&#x27;s avatar" class="post-avatar" src="img/common/ericf-avatar.png">
+
+                            <h2 class="post-title">{{ post.title }}</h2>
+
+                            <p class="post-meta">
+                                By <a class="post-author" href="#">Eric Ferraiuolo</a> under <a class="post-category post-category-js" href="#">JavaScript</a>
+                            </p>
+                        </header>
+
+                        <div class="post-description">
+                            <p>
+                            {{ post.content }}
+                            </p>
+                        </div>
+                    </section>
+
+                </div>
+            </div>
+        </div>
+        
+        <div id="general" class="footer">
+            <div class="pure-menu pure-menu-horizontal">
+                <ul>
+                    <li class="pure-menu-item" v-for="fbutton in fbuttons"><a v-bind:href="fbutton.href" class="pure-menu-link">{{ fbutton.text }}</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<script>
+    window.addEventListener('load', () => {
+        new Vue({
+            el: '#layout',
+            data:{
+                posts: [],
+                title: '',
+                description:'',
+                buttons:'',
+                fbuttons:'',
+                created_at:'',
+                updated_at:''
+            },
+            created(){
+
+                fetch('api/posts')
+                .then((res) => {return res.json()})
+                .then((res) => {
+                    this.posts = res;
+                })
+
+                fetch('api/general')
+                .then((res) => {return res.json()})
+                .then((res) => {
+                    this.title = res[0].title;
+                    this.description = res[0].description;
+                    this.buttons = JSON.parse(res[0].buttons);
+                    this.fbuttons = JSON.parse(res[0].fbuttons);
+                    this.created_at = res[0].created_at;
+                    this.updated_at = res[0].updated_at;
+                })
+            }
+        })
+    });
 </script>
 </body>
 </html>
