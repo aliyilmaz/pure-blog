8141c261c10fd4969236ea14f84a471ac0b718ca
aliyilmaz <aliyilmaz.work@gmail.com>
2019-03-05 16:10:58 +0300

    Syntax fixed.

--- app/samantha/get_data.php
+++ app/samantha/get_data.php
@@ -1,23 +1,23 @@
 <?php
 
-function get_data($table, $needle, $column=null){
-    
+function get_data($table, $needle, $column = null)
+{
     global $Mind;
     $data = array();
-    
-    if(!$Mind->is_column($table, $column)){
+
+    if (!$Mind->is_column($table, $column)) {
         $column = $Mind->increments($table);
     }
-    
+
     $schema = array(
-        'search'=>array(
-            'equal'=>array($column=>$needle)
-        )
+        'search' => array(
+            'equal' => array($column => $needle),
+        ),
     );
 
     $data = $Mind->get($table, $schema);
 
-    if(count($data)>1){
+    if (count($data) > 1) {
         return $data;
     }
 
