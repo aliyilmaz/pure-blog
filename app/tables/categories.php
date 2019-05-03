bc4dc5bb766bc5533fe30c6238f8e40be34af118
aliyilmaz <aliyilmaz.work@gmail.com>
2019-05-04 00:29:54 +0300

    Kategori içeriği test rotasından karşılanacaktır.

--- app/tables/categories.php
+++ app/tables/categories.php
@@ -8,40 +8,7 @@ $scheme = array(
     'description:small',
     'class_name:string',
     'created_at:string',
-    'updated_at:string'
+    'updated_at:string',
 );
 
 $this->createtable('categories', $scheme);
-
-// Added demo categories.
-$data = array(
-    array(
-        'text'=>'Uncategorized', 
-        'class_name'=>'post-category', 
-        'created_at'=>date('d-m-Y H:i:s')
-    ),
-    array(
-        'text'=>'CSS', 
-        'class_name'=>'post-category-design', 
-        'created_at'=>date('d-m-Y H:i:s')
-    ),
-    array(
-        'text'=>'Pure', 
-        'class_name'=>'post-category-pure', 
-        'created_at'=>date('d-m-Y H:i:s')
-    ),
-    array(
-        'text'=>'JavaScript', 
-        'class_name'=>'post-category-js', 
-        'created_at'=>date('d-m-Y H:i:s')
-    ),
-    array(
-        'text'=>'YUI', 
-        'class_name'=>'post-category-yui', 
-        'created_at'=>date('d-m-Y H:i:s')
-    )
-);
-
-$this->insert('categories', $data);
-
-?>
