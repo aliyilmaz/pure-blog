d60108b635a67e0665240057627bbae87e5baa3e
aliyilmaz <aliyilmaz.work@gmail.com>
2019-05-08 23:46:06 +0300

    Login sayfası güncellendi.

--- app/views/admin/login.php
+++ app/views/admin/login.php
@@ -1,1 +1,23 @@
-<h1>Welcome to Login Screen</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <base href="<?=$this->baseurl; ?>">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="public/admin/login.css">
+</head>
+<body>
+    <div class='form animated bounceIn'>
+    <h2>Login To Your Account</h2>
+    <form>
+        <input placeholder='Username' type='text'>
+        <input placeholder='Password' type='password'>
+        <button class='animated infinite pulse'>Login</button>
+    </form>
+    </div>
+
+</body>
+</html>
