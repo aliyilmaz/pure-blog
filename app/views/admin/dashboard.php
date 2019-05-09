b9d926bc9b8817c3ca94d64189f9f813482af032
aliyilmaz <aliyilmaz.work@gmail.com>
2019-05-10 00:51:42 +0300

    Basit bir Dashboard sayfası oluşturuldu.

--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -1,1 +1,14 @@
-<h1>Welcome to Dashboard Screen</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <base href="<?=$this->baseurl; ?>">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+</head>
+<body>
+  <h2>Welcome to Dashboard page.</h2>
+  <a href="admin/logout">Çıkış Yap</a>
+</body>
+</html>
